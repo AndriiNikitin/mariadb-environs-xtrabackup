7077aee01de6dcdbae7cf253f7ea18ddae72352a
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-09 01:01:36 +0200

    make wget less verbose

--- x-version/download.sh
+++ x-version/download.sh
@@ -2,12 +2,36 @@
 
 # example url  https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-2.4.4/binary/tarball/percona-xtrabackup-2.4.4-Linux-x86_64.tar.gz 
 
-FILE=https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-__version/binary/tarball/percona-xtrabackup-__version-Linux-x86_64.tar.gz
+file=https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-__version/binary/tarball/percona-xtrabackup-__version-Linux-x86_64.tar.gz
 
 mkdir -p __workdir/../_depot/x-tar/__version
 
+(
 cd __workdir/../_depot/x-tar/__version
-[[ -f $(basename $FILE) ]] || wget -nc $FILE && tar -zxf $(basename $FILE) --strip 1
-cd -
 
+function cleanup {
+  [ -z "$wgetpid" ] || kill "$wgetpid" 2>/dev/null
+}
+
+trap cleanup INT TERM
+
+if [ ! -f "$(basename $file)"  ] ; then
+  echo downloading "$file"
+  wget -q -np -nc $file &
+  wgetpid=$!
+  while kill -0 $wgetpid 2>/dev/null ; do
+    sleep 10
+    echo -n .
+  done
+  wait $wgetpid
+  wgetpid=""
+fi
+
+if [ -f "$(basename $file)" ] ; then
+  if [ ! -x bin/mysqld ] ; then
+    tar -zxf "$(basename $file)" --strip 1
+  fi
+fi
+)
+:
 
